73d8cc7b85e64f05848081b4540261c319979df9
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2023-12-30 23:13:58 +0530

    compiler

--- Program/Test.java
+++ Program/Test.java
@@ -1,14 +1,28 @@
-class Test{
-	public static void main(String [] args){
-		
-		System.out.println("Relation Operation ");
-		int a=10;
-	   double d=10.1;
-	   int c=20;
-	   boolean x=a>d;
-	   boolean y=d>c;
-	    boolean b=x==y;
-     	   System.out.println( b);//true
- 
-	}
+class Test {
+  public static void main(String[] args) {
+    
+    // create variables
+    int a = 7, b = 11;
+
+    // value of a and b
+    System.out.println("a is " + a + " and b is " + b);
+
+    // == operator
+    System.out.println(a == b);  // false
+
+    // != operator
+    System.out.println(a != b);  // true
+
+    // > operator
+    System.out.println(a > b);  // false
+
+    // < operator
+    System.out.println(a < b);  // true
+
+    // >= operator
+    System.out.println(a >= b);  // false
+
+    // <= operator
+    System.out.println(a <= b);  // true
+  }
 }
