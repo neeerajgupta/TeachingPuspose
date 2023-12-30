1ab069962740731786390f5ed0b8e2f0fc673750
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2023-12-30 18:03:15 +0530

    compiler

--- Program/Test.java
+++ Program/Test.java
@@ -1,15 +1,12 @@
 class Test{
 	public static void main(String [] args){
-		 
-   // 1)-PRE increments---(x=++z)
-   int x=10;
-   System.out.println("preeIncrement OperatoR :") ; 
-    System.out.println(++x);  //1+10===>10
-   
-//	2)-Post increments---(x=z++)");
-	int y=20;
-	 System.out.println("PostIncrement OperatoR :") ; 
-    System.out.println(y++);//20 
- System.out.println(y);	//20 -->+1==21
+		String s ="Shuhani";
+		int a=20;
+        int b=30;
+       int c=30;
+	   double d=10.5;
+       int e=Integer.parseInt(""+a+b+c);	   
+    System.out.println(e);//203030--->80
+ 
 	}
 }
