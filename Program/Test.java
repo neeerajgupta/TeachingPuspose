c0ac53226a9c9f9ce49b0845ee599015134ecff9
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2024-01-15 22:57:01 +0530

    nfjn

--- Program/Test.java
+++ Program/Test.java
@@ -1,11 +1,11 @@
  class Test{
 		 public static void main(String nrreakl[]){
-			 int a=10,b=20;
-			while(a<b){
-			System.out.println("Devanand");
-		
-			}	
-			System.out.println("hi");
+			 
+			do;
+			
+			
+			while(true);
+			
 			
 		 }
 	 }
